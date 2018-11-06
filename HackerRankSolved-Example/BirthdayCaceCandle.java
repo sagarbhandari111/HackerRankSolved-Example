14a04d212f2a4a4d8ea1491dc8f9a69bdf3ee9a9
Sagar Pundalik Bhandari <36851276+sagarbhandari111@users.noreply.github.com>
2018-11-06 13:05:03 +0530

    Create BirthdayCaceCandle.java

---
+++ HackerRankSolved-Example/BirthdayCaceCandle.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+        int i,len=ar.length,ctr=0;
+        int max=ar[0];
+        for(i=0;i<len;i++){
+            if(max<ar[i]){
+                max=ar[i];
+            }
+        }
+        for(i=0;i<len;i++){
+            if(ar[i]==max){
+                ctr=ctr+1;
+            }
+        }
+         //System.out.print(ctr);
+          return ctr;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
